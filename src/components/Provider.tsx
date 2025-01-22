5d64b223938c662bb3b1ab16009fb1dd7b032248
ZubairAnwer <zubairmohomad4@gmail.com>
2025-01-22 16:58:53 +0500

    Dynamic Frontend Components

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/products"
+      successUrl="https://comforty-ecommerce-marketplace.vercel.app/stripe/success"
+      cancelUrl="https://comforty-ecommerce-marketplace.vercel.app/products"
       currency="USD"
       billingAddressCollection={true}
       shouldPersist={true}
