87584e88344dc49d351330edd398ad6d3ae75558
Amraha <amrahaanwar@gmail.com>
2025-02-07 02:10:05 +0500

    ignore this-mistakenly pushed

--- amraha-marketplace/src/sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { productSchema } from "./products";
-import { categorySchema } from "./categories";
-import subscriberSchema from './subscriber';
-import orderSchema from './order';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema, subscriberSchema, orderSchema],
-};
